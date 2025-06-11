f230a2b2061bf16a338ecdb7651fd61a225068e0
paolo9688 <paolo.pz1@gmail.com>
2025-06-11 12:42:00 +0200

    created CalcolatriceController

---
+++ src/main/java/com/example/test_exercise/controller/CalcolatriceController.java
@@ -0,0 +1,45 @@
+package com.example.test_exercise.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalcolatriceController {
+
+    @GetMapping("/sum")
+    public Integer getSum(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer sum = a + b;
+        return sum;
+    }
+
+    @GetMapping("/sub")
+    public Integer getSub(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer sub = a - b;
+        return sub;
+    }
+
+    @GetMapping("/multip")
+    public Integer getMultip(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer multip = a * b;
+        return multip;
+    }
+
+    @GetMapping("/div")
+    public Double getDiv(@RequestParam Double a, @RequestParam Double b) {
+        Double div = a / b;
+        return div;
+    }
+
+    @GetMapping("/pow")
+    public Double getPow(@RequestParam Double a, @RequestParam Double b) {
+        Double pow = Math.pow(a, b);
+        return pow;
+    }
+
+    @GetMapping("/rad")
+    public Double getRad(@RequestParam Double a) {
+        Double rad = Math.sqrt(a);
+        return rad;
+    }
+}
