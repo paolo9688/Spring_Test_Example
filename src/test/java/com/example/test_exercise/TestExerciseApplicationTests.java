42bf996745bb42c98d73cef14501995140a65731
paolo9688 <paolo.pz1@gmail.com>
2025-06-12 10:57:34 +0200

    created sumTest()

--- src/test/java/com/example/test_exercise/TestExerciseApplicationTests.java
+++ src/test/java/com/example/test_exercise/TestExerciseApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.test_exercise;
 
+import com.example.test_exercise.controller.CalcolatriceController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@ActiveProfiles(value = "dev")
+@AutoConfigureMockMvc
 class TestExerciseApplicationTests {
 
+	@Autowired
+	private CalcolatriceController calcolatriceController;
+
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void sumTest() throws Exception{
+		this.mockMvc.perform(get("/api/calcolatrice/sum")
+				.param("a", "4")
+				.param("b", "2"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("6"));
 	}
+
 
 }
